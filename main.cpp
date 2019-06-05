6918a9fb6e6b171280a3fbfaa9834eb08f7e7b1a
lebllex <alexisvabel@gmail.com>
2019-06-05 17:23:00 +0700

    experiments with main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cstdarg>
 #include <iostream>
+#include <array>
 
 #include "commontypes.h"
 #include <range/v3/all.hpp>
@@ -11,22 +12,35 @@
 
 // typedef std::vector<std::vector<int> > IntIpVectorsT;
 auto is_one = [](std::vector<int> i) -> bool { return *i.begin() == 1; };
-
+auto print_each = [] (std::vector<int> vct) {
+    auto itbegin = vct.begin();
+    while(itbegin != vct.end()){
+        std::cout << *itbegin++;
+        if(itbegin != vct.end()) std::cout<<" ";
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc , char const *argv[] ){
     try{
         
-        IntIpVectorsT ip_pool = load_IntIpVectorT_stdin();
-
-        
-        std::sort(ip_pool.begin(), ip_pool.end(), std::greater<IntIpVectorT> () );
+        // IntIpVectorsT ip_pool = load_IntIpVectorT_stdin();        
+        // std::sort(ip_pool.begin(), ip_pool.end(), std::greater<IntIpVectorT> () );
 
         // first is one - means that got two pointers, begining of one and beginning of not one;
-        auto one_begin = ranges::find(ip_pool,is_one);
-        auto one_end   = ranges::find_if_not(ip_pool,is_one);
-        if(one_begin != ranges::end(ip_pool) &&  one_end != ranges::end(ip_pool)){
-            std::for_each(one_begin, one_end, print_vct);
-        }
+        // auto one_begin = ranges::find(ip_pool,is_one);
+        // auto one_end   = ranges::find_if_not(ip_pool,is_one);
+        // if(one_begin != ranges::end(ip_pool) &&  one_end != ranges::end(ip_pool)){
+        //     std::for_each(one_begin, one_end, print_vct);
+        // }
+        std::vector<int> vct{10,3,4,6,5,7} ;
+        std::vector<int> vct2{1,8,4,6,5,7} ;
+        std::cout << ranges::is_sorted(vct)  << std::endl;
+        std::cout << ranges::is_sorted(vct2) << std::endl;
+
+
+
+
 
 
         // IntIpVectorsT ip_poolOnes   = get_ip_filter_first(ip_pool,1);
